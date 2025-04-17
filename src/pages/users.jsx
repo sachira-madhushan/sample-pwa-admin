f4242b8360e9f909dd42c333f0d08ffeba97744f
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-04-17 09:27:52 +0530

    Load users from backend

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,25 +1,41 @@
 import React, { useEffect, useState } from 'react'
-
+import axios from 'axios';
 export default function UsersPanel() {
 
-    const [users,setUsers]=useState([
-        { id: 1, name: "John Doe", email: "john@example.com", created_at: "2023-01-01", status: "Active" },
-        { id: 2, name: "Jane Smith", email: "jane@example.com", created_at: "2023-02-01", status: "Inactive" },
-    ]);
+    const [users, setUsers] = useState([]);
+
+    // const [users,setUsers]=useState([
+    //     { id: 1, name: "John Doe", email: "john@example.com", created_at: "2023-01-01", status: "Active" },
+    //     { id: 2, name: "Jane Smith", email: "jane@example.com", created_at: "2023-02-01", status: "Inactive" },
+    // ]);
+
+    useEffect(() => {
+        fetchUsers();
+    })
+
+    const fetchUsers = async () => {
+        try {
+            const response = await axios.get("http://localhost:4000/api/v1/admin/users");
+            setUsers(response.data);
+        } catch (error) {
+            console.error("Error fetching users:", error);
+        }
+
+    }
 
     const handleActiveInactive = (id) => {
         const targetUser = users.find((user) => user.id === id);
-        const newStatus = targetUser.status === "Active" ? "Inactive" : "Active";
-    
+        const newStatus = targetUser.status === 1 ? 0 : 1;
+
         setUsers((prevUsers) =>
             prevUsers.map((user) =>
                 user.id === id ? { ...user, status: newStatus } : user
             )
         );
-    
+
         alert(`User with ID ${id} has been ${newStatus.toLowerCase()}.`);
     };
-    
+
     return (
 
         <div>
@@ -45,11 +61,11 @@ export default function UsersPanel() {
                                 {
                                     user.status === "Active" ? (
                                         <td className="px-4 py-2 border-b ">
-                                            <button className='rounded bg-green-500 text-white px-4 py-1' onClick={()=>handleActiveInactive(user.id)}>{user.status}</button>
+                                            <button className='rounded bg-green-500 text-white px-4 py-1' onClick={() => handleActiveInactive(user.id)}>{user.status==1?"Active":"Inactive"}</button>
                                         </td>
                                     ) : (
                                         <td className="px-4 py-2 border-b">
-                                            <button className='rounded bg-red-500 text-white px-4 py-1' onClick={()=>handleActiveInactive(user.id)}>{user.status}</button>
+                                            <button className='rounded bg-red-500 text-white px-4 py-1' onClick={() => handleActiveInactive(user.id)}>{user.status==1?"Active":"Inactive"}</button>
                                         </td>
                                     )
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3030,
+  },
 })
